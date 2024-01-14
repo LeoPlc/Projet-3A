6c34edece55210d93d9cd641caf916919439cb92
Leonardo <palaric.leo@gmail.com>
2024-01-14 19:01:09 +0100

    udpate application

--- prototype/python_app/yeelight_func.py
+++ prototype/python_app/yeelight_func.py
@@ -4,6 +4,15 @@ import tkinter as tk
 import tkinter.messagebox
 
 
+'''
+Besoin d'une nouvelle variable permettant de prendre en compte l'objet auquel on souhaite se connecter. Plus encore, 
+il faudrait faire en sorte de pouvoir choisir ultérieurement à la découverte des IoT disponibles, qu'il faudrait insérer 
+plutôt directement dans la liste au lieu d'avoir une nouvelle fenêtre. Idée: udpate_iot_list() une fois les appareils découverts
+en y intégrant non pas l'adresse IP mais plutôt le nom associé à l'appareil, avec un bouton permettant d'avoir toute une liste d'informations
+à propos de l'objet disponible (état, température humidité, etc...).
+'''
+
+#recherche d'IoT disponibles (ici bulbes pour la librairie yeelight pour les démo)
 
 def lookfor_available_bulbs(available_bulbs):
     # available_bulbs = yeelight.discover_bulbs()
@@ -15,8 +24,11 @@ def lookfor_available_bulbs(available_bulbs):
         bulbs_window.title("Available bulbs:")
         bulbs_window.geometry("200x300")
         
-        
-        
         bulbs_window.mainloop()
         
-
+def connect_available_bulb():
+    #choix du bulbe
+    
+    #connexion à au bulbe choisi
+    
+    #choix de l'action à y faire
